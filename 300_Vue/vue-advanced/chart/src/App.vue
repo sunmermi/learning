97fff290cbe89331452ea117be4f538869e61c8d
sseom <ysum1011@gmail.com>
2020-07-08 15:57:10 +0900

    LV.3 - Vue.js 완벽 가이드 : 외부 라이브러리 모듈화 방법(차트)
    섹션 16. 외부 라이브러리 모듈화 방법(차트)
    - 라이브러리 모듈화의 이유와 배경
    - 차트 라이브러리 설치 및 차트 그리기

---
+++ 300_Vue/vue-advanced/chart/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <h2>Chart.js</h2>
+    <canvas id="myChart" width="400" height="400"></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from 'chart.js';
+
+export default {
+  // 컴포넌트 속성, 인스턴그 옵션
+  name: 'App',
+  mounted() {
+    var ctx = document.getElementById('myChart');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+    console.log('myChart : ', myChart);
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
